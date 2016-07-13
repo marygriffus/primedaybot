3f13bab3ffc00faa922921abf06fb51aff30893a
Mary Griffus <mary.e.shultz@gmail.com>
2016-07-13 16:47:24 -0400

    initial commit. based on heroku_ebooks bot

---
+++ local_settings.py
@@ -0,0 +1,15 @@
+'''
+PrimeDay2
+'''
+
+#configuration
+MY_CONSUMER_KEY = 'rTLqThF8NjIDUmsC52YDIQTyF'
+MY_CONSUMER_SECRET = 'tu8M6BCPHwPkT3AfPOpAIdP5idKDt3g5JqU6dU7tWmnwhL0wdJ'
+MY_ACCESS_TOKEN_KEY = '	753266181180682240-vZVJmXX62eRpbIpJyWfAfFkYP04yI48'
+MY_ACCESS_TOKEN_SECRET = '3IWmVCEQnhSoPA90XIHRPZyx3xRwfhQLMVY68nn5pJ58l'
+
+ODDS = 8 #How often do you want this to run? 1/8 times?
+DEBUG = True #Set this to False to start Tweeting live
+STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
+TEST_SOURCE = '.txt' #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
+TWEET_ACCOUNT = '' #The name of the account you're tweeting to.
